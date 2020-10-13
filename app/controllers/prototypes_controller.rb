d4aabb9076405d1da77f5f9e17798880bdaf0863
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-10-13 15:12:15 +0900

    アクションとルーティングを設定しましょう
    local3000表示
    
    rototypesコントローラーにeditアクションとupdateアクションを設定した（まだアクション内の処理は書かない）
    resourcesを用いて、上記で設定したeditアクションとupdateアクションに対するルーティングをroutes.rbに記述した
    rails routesを実行して、ルーティングが正しく設定できていることを確かめた
    
    上記完了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,12 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def prototype_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: [:new, :create, :show]
+  resources :prototypes, only: [:new, :create, :show,:edit, :update]
 end
