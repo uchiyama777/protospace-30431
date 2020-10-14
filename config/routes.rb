74f118736398836b0221541c3ff2139f5d7124cd
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-10-14 11:14:32 +0900

    コントローラーを作成し、アクションとルーティングを設定しましょう
    ローカル表示
    
    commentsコントローラーを作成した
    commentsコントローラーにcreateアクションを設定した（アクション内の処理は書かない）
    resourcesを用いて、上記で設定したcreateアクションに対するルーティングをroutes.rbに記述した
    rails routesを実行して、ルーティングが正しく設定できていることを確かめた
    
    上記完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: [:new, :create, :show,:edit, :update,:destroy]
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  resources :comments, only: :create
 end
