dac443119d8403647859ccd35a94900c383dca83
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-10-14 10:51:24 +0900

    Commentモデルおよびテーブルを作成しましょう
    ローカル表示
    
    rails g model commentでCommentモデルを作成した
    マイグレーションファイルに、テキストのためのカラムを追加した
    マイグレーションファイルに、userおよびprototypeを参照するための外部キーを記述した（references型を用いる）
    rails db:migrateを実行し、Sequel Proでcommentsテーブルが存在することを確かめた
    Commentモデルに、userおよびprototypeに対するアソシエーションを設定した
    UserモデルおよびPrototypeモデルにも、commentに対するアソシエーションを設定した（必要に応じてdependent: :destroyオプションを使用した）
    
    上記完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :prototypes
+  has_many :comments
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_121416) do
+ActiveRecord::Schema.define(version: 2020_10_13_120111) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_10_11_121416) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "prototype_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["prototype_id"], name: "index_comments_on_prototype_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -60,4 +70,6 @@ ActiveRecord::Schema.define(version: 2020_10_11_121416) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
 end
